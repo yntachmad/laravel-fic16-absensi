e60deff5dadec9535ebb8176e6661d4879092763
yntachmad <achmad.t.jayanto@gmail.com>
2024-09-07 19:46:52 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.dashboard',['type_menu' => 'dashboard']);
+
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+})->name('login');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/', function () {
+        return view('pages.dashboard',['type_menu' => 'dashboard']);
+    })->name('home');
+
 });
